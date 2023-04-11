42b4efc5cc852f9628c17670c3e947641d9955c6
npgarima <thegarimaweb@gmail.com>
2023-04-11 13:12:13 +0545

    added some updates

--- first.php
+++ first.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>GITS </title>
 </head>
 <body>
-    <?php
-    echo" This works for every utility class in the framework, which means you can change literally anything at a given breakpoint — even things like letter spacing or cursor styles.
 
-    Here’s a simple example of a marketing page component that uses a stacked layout on small screens, and a side-by-side layout on larger screens (resize your browser to see it in action):
-    
-    "
+<h2>Git branch</h2>
+<p>
+<div class="first">
+    GITHUB MANAGEMENT~
+</div>
+
+</p>
     ?>
 </body>
 </html>
